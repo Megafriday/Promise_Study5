66095a360cbc65aeeddafa09bebb07558b42679d
Megafriday <megafriday@yahoo.co.jp>
2020-10-08 18:47:16 +0900

    Update app2.js

--- app2.js
+++ app2.js
@@ -4,13 +4,13 @@ const fileName = './test.txt';
 
 async function main() {
 	for (let count = 0; count < 30; count++) {
-		await syncFunction("おはようございます\n");
-		await syncFunction("こんにちは\n");
-		await syncFunction("こんばんは\n");
+		await promiseFunction("おはようございます\n");
+		await promiseFunction("こんにちは\n");
+		await promiseFunction("こんばんは\n");
 	}
 }
 
-function syncFunction(str) {
+function promiseFunction(str) {
 	return new Promise((resolve, rejct) => {
 		fs.appendFile(fileName, str, 'utf8', () => { resolve(); });
 	});
